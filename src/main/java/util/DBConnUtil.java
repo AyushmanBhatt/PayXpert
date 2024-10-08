9ed9a2ec83653a7069af105762460f3848b35c09
Ayushman Bhatt <103947002+AyushmanBhatt@users.noreply.github.com>
2024-10-08 16:34:27 +0530

    Update DBConnUtil.java
    Encrypting root user password.

--- src/main/java/util/DBConnUtil.java
+++ src/main/java/util/DBConnUtil.java
@@ -6,7 +6,7 @@ public class DBConnUtil {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Update with your database driver
             String url = "jdbc:mysql://localhost:3306/payxpert";
             String username = "root";
-            String password = "ayush7983";
+            String password = /*user password*/;
 
             return DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException e) {
